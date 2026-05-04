d463858d80c83877933a93395e60d952096d55b4
Danil Mokin <mokich45usa@gmail.com>
2026-05-04 14:32:01 -0700

    fix next tracing config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,5 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../../.."),
   images: {
     remotePatterns: [
       {
